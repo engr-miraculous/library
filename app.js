570ef3e737f44391e897b36c351c0077877fee43
miracle <miraclousyh2000@yahoo.com>
2018-09-08 21:44:29 +0200

    used arrow function

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/popper.js/dist')));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 });
-app.listen(3000, function () {
+app.listen(3000, () => {
   debug(`listening on port ${chalk.green('3000')} `);
 });
