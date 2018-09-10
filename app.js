5f439389e88506e194f92b04ea92041fd2ed5260
miracle <miraclousyh2000@yahoo.com>
2018-09-10 06:55:07 +0200

    rendering engine added

--- app.js
+++ app.js
@@ -5,17 +5,19 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/popper.js/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.render('index', { list: ['a', 'b'], title: 'library' });
 });
-app.listen(3000, () => {
-  debug(`listening on port ${chalk.green('3000')} `);
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)} `);
 });
